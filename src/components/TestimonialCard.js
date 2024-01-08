5a37f6ef2536390d39068ed636db8fcd236b0913
GitHubParth <parthpadhiar2002@gmail.com>
2024-01-08 19:17:27 +0530

    Video Compressed

--- src/components/TestimonialCard.js
+++ src/components/TestimonialCard.js
@@ -121,11 +121,7 @@ const TestimonialCard = ({
 					}}
 					className="absolute top-3 right-3 md:hidden block z-10"
 				>
-					<svg
-						fill="currentColor"
-						className="w-4 h-4"
-						viewBox="0 0 16 16"
-					>
+					<svg fill="currentColor" className="w-4 h-4" viewBox="0 0 16 16">
 						<path d="M2.146 2.854a.5.5 0 1 1 .708-.708L8 7.293l5.146-5.147a.5.5 0 0 1 .708.708L8.707 8l5.147 5.146a.5.5 0 0 1-.708.708L8 8.707l-5.146 5.147a.5.5 0 0 1-.708-.708L7.293 8z" />
 					</svg>
 				</button>
